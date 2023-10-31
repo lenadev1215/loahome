241ab135e4159ff2a92ac6ac5f328a357d2def8b
user\토마토 <lenadev7923@gmail.com>
2023-10-31 16:42:39 +0900

    api 키 입력창 추가

---
+++ src/js/components/form/APIkeyForm.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const APIkeyForm = ({ onChange, onSubmit }) => {
+  return (
+    <div className="form__api">
+      <form onSubmit={onSubmit}>
+        <input type="text" className="input__text key" placeholder="로스트아크 API KEY를 입력해주세요." onChange={onChange} />
+        <button 
+          type="submit" 
+          className="btn btn__submit"
+        >로그인
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default APIkeyForm;
---
+++ src/js/containers/APIkeyFormContainer.jsx
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import APIkeyForm from '../components/form/APIkeyForm';
+
+const APIkeyFormContainer = ({ i }) => {
+  const [ key, setKey ] = useState('');
+  const [ keyInit, setKeyInit ] = useState([]);
+
+  // keyInit 초기화
+  useEffect(() => {
+    const k = localStorage.getItem('apiKey');
+
+    if ( Array.isArray(k) ) {
+      setKeyInit(JSON.parse(k));
+    } else {
+      setKeyInit([]);
+    }
+  }, []);
+
+  const onChange = e => {
+    const { value } = e.target;
+    setKey( value );
+  }
+
+  const onSubmit = e => {
+    e.preventDefault();
+    
+    keyInit[i] = key;
+    localStorage.setItem('apiKey', JSON.stringify(keyInit));
+  }
+
+  return (
+    <APIkeyForm
+      onChange={onChange}
+      onSubmit={onSubmit}
+    />
+  );
+};
+
+export default APIkeyFormContainer;
